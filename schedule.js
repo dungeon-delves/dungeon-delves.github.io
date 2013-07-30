1e2eb95ff3a8556018d9370ec0811a3d7bc9b8a4
Sven Eigenbrodt <eigenbrodt@gmail.com>
2013-07-30 14:40:40 +0200

    explicitly sum up event durations

--- schedule.js
+++ schedule.js
@@ -1,15 +1,46 @@
 var DD = function () {
-    var lastKnownDDInMsec = new Date("07/29/2013 18:00").getTime();
-    var hoursBetweenDDs = 6;
+    //come time-constants
     var msecPerMinute = 1000 * 60;
     var msecPerHour = msecPerMinute * 60;
     var msecPerDay = msecPerHour * 24;
 
+    //rotationEvents: map[eventname, duration]
+    var rotationEvents = {};
+    rotationEvents['dungeon-delves'] = 1;
+    rotationEvents['foundry'] = 1;
+    rotationEvents['gauntlgrym1'] = 0.5;
+    rotationEvents['gauntlgrym2'] = 0.5;
+    rotationEvents['gauntlgrym3'] = 1;
+    rotationEvents['professions'] = 1;
+    rotationEvents['skirmishes'] = 1;
+    rotationEvents['pvp'] = 1;
+
+    var mapValueToArray = function (mapObj) {
+        var array = new Array();
+        for (var key in rotationEvents) {
+            if (rotationEvents.hasOwnProperty(key)) {
+                var value = rotationEvents[key];
+                array.push(value);
+            }
+        }
+        return array;
+    }
+
+    var sumValues = function (array) {
+        return array.reduce(function(previousValue, currentValue){
+            return previousValue + currentValue;
+        }); 
+    }
+
     return {
         nextDD: function () { 
+            //set what we know
+            var lastKnownDDInMsec = new Date("07/29/2013 18:00").getTime();
             var currentTimeInMsec = new Date().getTime();
+            var rotationLengthInHours = sumValues(mapValueToArray(rotationEvents));
+
+            //calculate time since last known DD.
             var interval = currentTimeInMsec - lastKnownDDInMsec;
-            //calculate time sind last known DD.
             var hours = Math.floor(interval / msecPerHour );
             interval = interval - (hours * msecPerHour );
             var minutes = Math.floor(interval / msecPerMinute );
@@ -17,8 +48,8 @@ var DD = function () {
             var seconds = Math.floor(interval / 1000 );
 
             //calculate when the next DD should be
-            passedDDsSince = Math.floor(hours / hoursBetweenDDs) ;
-            targetHours = (passedDDsSince + 1) * hoursBetweenDDs
+            passedDDsSince = Math.floor(hours / rotationLengthInHours);
+            targetHours = (passedDDsSince + 1) * rotationLengthInHours;
 
             //calculate countdown
             hours = targetHours - hours;
@@ -29,7 +60,6 @@ var DD = function () {
             }
             seconds = 60 -seconds;
             return (hours + " hours, " + minutes + " minutes, " + seconds + " seconds.");
-
         }
     }
 }
